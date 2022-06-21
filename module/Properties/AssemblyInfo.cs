752eed4b39dde97101a0c96abfceb9c4827c1f8a
temeddix <temeddix@gmail.com>
2022-06-21 20:20:13 +0900

    Version 1.6

--- module/Properties/AssemblyInfo.cs
+++ module/Properties/AssemblyInfo.cs
@@ -9,11 +9,10 @@ using System.Runtime.InteropServices;
     [assembly: Guid("95ABCCA2-5CE3-486A-9592-32464857F282")]
 #endif
 
-[assembly: AssemblyTitle("RhinoWASD")]
+[assembly: AssemblyTitle("Walkie")]
 [assembly: AssemblyDescription("Game-like navigation for Rhinoceros")]
-[assembly: AssemblyCompany("blickfeld7")]
-[assembly: AssemblyProduct("RhinoWASD")]
-[assembly: AssemblyCopyright("© 2019 blickfeld7.com")]
+[assembly: AssemblyCompany("Cunarist")]
+[assembly: AssemblyProduct("Walkie")]
+[assembly: AssemblyCopyright("© 2022 Cunarist")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyInformationalVersion("2")]   // make compatible with Rhino Installer Engine
+[assembly: AssemblyVersion("1.6")]
