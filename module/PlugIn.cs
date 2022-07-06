ed937f219d8319be179fcf926e42eac04bb7fdef
temeddix <temeddix@gmail.com>
2022-07-06 20:15:31 +0900

    Auto zoom target feature

--- module/PlugIn.cs
+++ module/PlugIn.cs
@@ -1,9 +1,82 @@
-﻿namespace RhinoWASD
+﻿using Rhino.Display;
+using Rhino.DocObjects;
+using Rhino.Geometry;
+using Rhino.PlugIns;
+using System;
+using System.Windows.Forms;
+
+namespace RhinoWASD
 {
     public class PlugIn : Rhino.PlugIns.PlugIn
     {
+
+        private static Timer timer;
+        private static System.Drawing.Point cursorPosition = Cursor.Position;
+
         public PlugIn() { Instance = this; }
 
         public static PlugIn Instance { get; private set; }
+
+        protected override LoadReturnCode OnLoad(ref string errorMessage)
+        {
+            timer = new Timer();
+            timer.Interval = 1;
+            timer.Tick += DetectMouseMove;
+            timer.Start();
+            return LoadReturnCode.Success;
+        }
+
+        protected override void OnShutdown()
+        {
+            timer.Stop();
+            timer.Tick -= DetectMouseMove;
+            timer.Dispose();
+            timer = null;
+        }
+
+        private static void DetectMouseMove(object sender, EventArgs args)
+        {
+            System.Drawing.Point newPosition = Cursor.Position;
+            if (newPosition.X != cursorPosition.X || newPosition.Y != cursorPosition.Y)
+                HandleMouseMove();
+            cursorPosition = newPosition;
+        }
+
+        private static void HandleMouseMove()
+        {
+            RhinoView view = Rhino.RhinoDoc.ActiveDoc.Views.ActiveView;
+            if (view == null)
+                return;
+            System.Drawing.Point cursorInView = view.ActiveViewport.ScreenToClient(Cursor.Position);
+            Point3d cameraLocation = view.ActiveViewport.CameraLocation;
+            Line viewLine = view.ActiveViewport.ClientToWorld(cursorInView);
+            viewLine.Extend(1E8, 0);
+            Point3d zoomTarget = viewLine.From;
+            bool didSetTarget = false;
+            foreach (RhinoObject eachObject in Rhino.RhinoDoc.ActiveDoc.Objects)
+            {
+                MeshType blankMesh = new MeshType();
+                Mesh[] meshes = eachObject.GetMeshes(blankMesh);
+                foreach (Mesh mesh in meshes)
+                {
+                    int[] faceIds;
+                    Point3d[] pinPoints = Rhino.Geometry.Intersect.Intersection.MeshLine(mesh, viewLine, out faceIds);
+                    foreach (Point3d pinPoint in pinPoints)
+                    {
+                        double beforeDistance = zoomTarget.DistanceTo(cameraLocation);
+                        double newDistance = pinPoint.DistanceTo(cameraLocation);
+                        if (newDistance < beforeDistance)
+                        {
+                            zoomTarget = pinPoint;
+                            didSetTarget = true;
+                        }
+                    }
+                }
+            }
+            if (didSetTarget)
+            {
+                view.ActiveViewport.SetCameraTarget(zoomTarget, false);
+            }
+        }
     }
 }
