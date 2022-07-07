933263b9417f98717d6e53f11454f34e3ee343fe
temeddix <temeddix@gmail.com>
2022-07-07 13:44:24 +0900

    Version 2.2

--- offer/upload.bat
+++ offer/upload.bat
@@ -1,5 +1,5 @@
 cd "..\distribution\Release"
 "C:\Program Files\Rhino 7\System\Yak.exe" login
-"C:\Program Files\Rhino 7\System\Yak.exe" push walkie-2.1.0-rh7_19-any.yak
+"C:\Program Files\Rhino 7\System\Yak.exe" push walkie-2.2.0-rh7_19-any.yak
 "C:\Program Files\Rhino 7\System\Yak.exe" search --all walkie
 pause
