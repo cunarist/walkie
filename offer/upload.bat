763d580fe6bf8cb8a22460ff9627c888ad41fd3a
temeddix <temeddix@gmail.com>
2022-07-07 16:33:33 +0900

    Modify upload procedure

--- offer/upload.bat
+++ offer/upload.bat
@@ -1,4 +1,4 @@
-cd "..\distribution\Release"
+cd "..\distribution"
 "C:\Program Files\Rhino 7\System\Yak.exe" login
 "C:\Program Files\Rhino 7\System\Yak.exe" push walkie-2.2.0-rh7_19-any.yak
 "C:\Program Files\Rhino 7\System\Yak.exe" search --all walkie
