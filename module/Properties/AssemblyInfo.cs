54c713012d19a7208c90af132236642eca3d7cae
temeddix <temeddix@gmail.com>
2022-07-23 14:30:01 +0900

    Version 2.9

--- module/Properties/AssemblyInfo.cs
+++ module/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Walkie")]
 [assembly: AssemblyCopyright("© 2022 Cunarist")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.7")]
+[assembly: AssemblyVersion("2.9")]
