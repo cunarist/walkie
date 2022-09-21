ba03d9a02586b7a143a8ee2cb2b3a4cd54e1d389
temeddix <temeddix@gmail.com>
2022-09-21 22:05:04 +0900

    Version 3.7

--- module/Properties/AssemblyInfo.cs
+++ module/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Walkie")]
 [assembly: AssemblyCopyright("© 2022 Cunarist")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.6")]
+[assembly: AssemblyVersion("3.7")]
