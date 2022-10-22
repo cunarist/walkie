e8fad5208af6731a9763f252ec1d1f0eecf253b9
temeddix <temeddix@gmail.com>
2022-10-23 01:45:50 +0900

    Version 4.4

--- module/Properties/AssemblyInfo.cs
+++ module/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Walkie")]
 [assembly: AssemblyCopyright("© 2022 Cunarist")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.3")]
+[assembly: AssemblyVersion("4.4")]
