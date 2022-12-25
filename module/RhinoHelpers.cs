1915b00ed33190162ee40cf163d4a05bd2c4d752
temeddix <temeddix@gmail.com>
2022-12-26 04:29:53 +0900

    Add convenient named view cycle

--- module/RhinoHelpers.cs
+++ module/RhinoHelpers.cs
@@ -8,16 +8,21 @@ namespace RhinoWASD
 {
     public static class RhinoHelpers
     {
-        public static string CurrentNamedView = string.Empty;
-
-        private static void RestoreNamedView(int index)
+        private static void RestoreNamedView(int index, bool showMessage = true)
         {
             if (index < 0 || index >= RhinoDoc.ActiveDoc.NamedViews.Count)
                 return;
 
             RhinoDoc.ActiveDoc.NamedViews.Restore(index, RhinoDoc.ActiveDoc.Views.ActiveView.MainViewport);
-            CurrentNamedView = RhinoDoc.ActiveDoc.NamedViews[index].Name;
-            Overlay.ShowMessage(CurrentNamedView);
+            string currentNamedView = RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name;
+
+            if (showMessage) { Overlay.ShowMessage("Named view \"" + currentNamedView + "\" restored"); }
+        }
+
+        private static void DisableNamedView(bool showMessage = true)
+        {
+            RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name = "Perspective";
+            if (showMessage) { Overlay.ShowMessage("Named view disabled"); }
         }
 
         public static void PreviousNamedView()
@@ -26,14 +31,18 @@ namespace RhinoWASD
             if (count < 1)
                 return;
 
-            if (!string.IsNullOrEmpty(CurrentNamedView))
+            string currentNamedView = RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name;
+            int cur = RhinoDoc.ActiveDoc.NamedViews.FindByName(currentNamedView);
+            int newCur = cur - 1;
+            if (cur == -1) { newCur = count - 1; }
+
+            if (newCur >= 0 && newCur < count)
             {
-                int cur = RhinoDoc.ActiveDoc.NamedViews.FindByName(CurrentNamedView);
-                int newCur = cur - 1;
-                if (newCur >= 0 && newCur < count)
-                {
-                    RestoreNamedView(newCur);
-                }
+                RestoreNamedView(newCur);
+            }
+            else
+            {
+                DisableNamedView();
             }
         }
 
@@ -43,14 +52,18 @@ namespace RhinoWASD
             if (count < 1)
                 return;
 
-            if (!string.IsNullOrEmpty(CurrentNamedView))
+            string currentNamedView = RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name;
+            int cur = RhinoDoc.ActiveDoc.NamedViews.FindByName(currentNamedView);
+            int newCur = cur + 1;
+            if (cur == -1) { newCur = 0; }
+
+            if (newCur >= 0 && newCur < count)
             {
-                int cur = RhinoDoc.ActiveDoc.NamedViews.FindByName(CurrentNamedView);
-                int newCur = cur + 1;
-                if (newCur >= 0 && newCur < count)
-                {
-                    RestoreNamedView(newCur);
-                }
+                RestoreNamedView(newCur);
+            }
+            else
+            {
+                DisableNamedView();
             }
         }
 
@@ -60,12 +73,12 @@ namespace RhinoWASD
             int result = RhinoDoc.ActiveDoc.NamedViews.Add(name, RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewportID);
             if (result >= 0)
             {
-                Overlay.ShowMessage("\"" + name + "\" saved as named view");
-                CurrentNamedView = name;
+                Overlay.ShowMessage("Named view \"" + name + "\" saved");
+                RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name = name;
             }
             else
             {
-                Overlay.ShowMessage("Couln't save view \"" + name + "\"");
+                Overlay.ShowMessage("Named view cannot be saved");
             }
         }
 
@@ -75,14 +88,32 @@ namespace RhinoWASD
             if (count < 1)
                 return;
 
-            bool result = RhinoDoc.ActiveDoc.NamedViews.Delete(CurrentNamedView);
+            string currentNamedView = RhinoDoc.ActiveDoc.Views.ActiveView.ActiveViewport.Name;
+            int cur = RhinoDoc.ActiveDoc.NamedViews.FindByName(currentNamedView);
+            int newCur = cur - 1;
+
+            bool shouldAlternate = false;
+            if (newCur >= 0 && newCur < count)
+            {
+                shouldAlternate = true;
+            }
+
+            bool result = RhinoDoc.ActiveDoc.NamedViews.Delete(currentNamedView);
             if (result)
             {
-                Overlay.ShowMessage("Named view \"" + CurrentNamedView + "\" deleted");
+                Overlay.ShowMessage("Named view \"" + currentNamedView + "\" deleted");
+                if (shouldAlternate)
+                {
+                    RestoreNamedView(newCur, false);
+                }
+                else
+                {
+                    DisableNamedView(false);
+                }
             }
             else
             {
-                Overlay.ShowMessage("Cannot delete named view \"" + CurrentNamedView + "\"");
+                Overlay.ShowMessage("Named view cannot be deleted");
             }
         }
 
